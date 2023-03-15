d8a6b700be9b44b9fd285bfe2c3474a7321da9de
Even Koshliak <evenkoshliak@o-s-i.org>
2023-03-15 18:04:58 +0200

    feature - add material-ui, react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Home } from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      app
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
