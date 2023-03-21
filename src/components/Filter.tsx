b8314f9f51a3b2256ba81b39a09e598f8ecb5f7f
Even Koshliak <evenkoshliak@o-s-i.org>
2023-03-21 15:26:30 +0200

    bugfix - remove form wrapper

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,31 +1,29 @@
-import { Box, debounce, InputAdornment, TextField } from '@mui/material';
+import { debounce, InputAdornment, TextField } from '@mui/material';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import { useState } from 'react';
 
 export const Filter = () => {
   const [filter, setFilter] = useState();
+
   const handleChange = (e: any) => {
     setFilter(e.target.value.trim());
   }
 
   return (
-    <Box component="form" autoComplete="off" className="w-full">
-      <TextField
-        variant="standard"
-        label="Filter news"
-        size="small"
-        color="secondary"
-        className="w-full"
-        onChange={debounce(handleChange, 500)}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <ManageSearchIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      {filter}
-    </Box>
+    <TextField
+      variant="standard"
+      label="Filter news"
+      size="small"
+      color="secondary"
+      className="w-full"
+      onChange={debounce(handleChange, 500)}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <ManageSearchIcon />
+          </InputAdornment>
+        ),
+      }}
+    />
   )
 }
