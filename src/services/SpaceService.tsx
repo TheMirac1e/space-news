47ad775e3990f21ed882ad64f35688bc0c71956f
Even Koshliak <evenkoshliak@o-s-i.org>
2023-04-13 22:52:48 +0300

    feature - add transform news list

--- src/services/SpaceService.tsx
+++ src/services/SpaceService.tsx
@@ -5,7 +5,17 @@ export const useSpaceService = () => {
 
   const getAllNews = async () => {
     const res = await request('https://api.spaceflightnewsapi.net/v4/articles/?limit=9');
-    return res.data.results;
+    return res.results.map(_transformNewsList);
+  }
+
+  const _transformNewsList = (news: any) => {
+    return {
+      id: news.id,
+      title: news.title,
+      subtitle: news.summary,
+      image: news.image_url,
+      date: news.published_at
+    }
   }
 
   return { loading, error, request, clearError, getAllNews };
