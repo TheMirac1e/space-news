f7626845c0f4c3519cbbc4261f25b858cfa28375
Even Koshliak <evenkoshliak@o-s-i.org>
2023-05-17 17:45:31 +0300

    bugfix - remove material import from main

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './index.scss'
-import { StyledEngineProvider } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <StyledEngineProvider injectFirst>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </StyledEngineProvider>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
