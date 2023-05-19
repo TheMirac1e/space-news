08ddbb1b2d18de1313a388be18f225f551556bd4
Even Koshliak <evenkoshliak@o-s-i.org>
2023-05-19 18:37:10 +0300

    bugfix - remove useEffect from filter

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,24 +1,18 @@
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
-import { useEffect } from 'react';
 import { IFilter } from '../types/types';
 
 // add filter by name and change styles
 export const Filter = ({ ...props }: IFilter) => {
-  const { filter, setFilter } = props;
+  const { setFilter } = props;
 
   const handleChange = (e: any) => {
     setFilter(e.target.value.trim());
   }
 
-  useEffect(() => {
-
-
-  }, [filter])
-
   return (
     <div className="flex flex-col mb-5">
       <label htmlFor="filter">Filter news</label>
-      <div className="relative">
+      <div className="relative flex">
         <ManageSearchIcon className="absolute top-1/2 translate-y-[-50%] left-2" />
         <input className="w-full h-8 border-b-1 border-indigo-500 border-t-none pl-9" type="text" id="filter" onChange={handleChange} ></input>
       </div>
