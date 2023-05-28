1dec5679b86fa0a84b022d6dfe2056ff9587661c
Even Koshliak <evenkoshliak@o-s-i.org>
2023-05-29 00:03:08 +0300

    bugfix - fix base url

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Details />} />
+        <Route path="/news" element={<Home />} />
+        <Route path="/news/:id" element={<Details />} />
       </Routes>
     </div>
   )
